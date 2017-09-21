90caf250878b30bcff8ec6df57e573e86bb90c32
Nicholas Frush <nicholas.frush@gmail.com>
2017-09-21 18:17:10 -0400

    Updated proto3 schema

--- user_service_server/main.go
+++ user_service_server/main.go
@@ -4,9 +4,9 @@ import (
 	"log"
 	"net"
 
+	pb "github.com/nfrush/user_service/user_service_pb"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	pb "github.com/nfrush/user_service/user_service_pb/user_service"
 	"google.golang.org/grpc/reflection"
 )
 
@@ -17,7 +17,35 @@ const (
 type server struct{}
 
 func (s *server) CreateAccount(ctx context.Context, request *pb.CreateAccountRequest) (*pb.CreateAccountResponse, error) {
-	return nil, &pb.CreateAccountResponse{StatusType: 1, StatusMessage: "This is a test message", nil
+	return &pb.CreateAccountResponse{StatusType: 1, StatusMessage: "This is a test message"}, nil
+}
+
+func (s *server) UpdateInfo(ctx context.Context, request *pb.UpdateInfoRequest) (*pb.UpdateInfoResponse, error) {
+	return &pb.UpdateInfoResponse{StatusType: 1, StatusMessage: "This is a test message"}, nil
+}
+func (s *server) DeleteAccount(ctx context.Context, request *pb.DeleteAccountRequest) (*pb.DeleteAccountResponse, error) {
+	return &pb.DeleteAccountResponse{StatusType: 1, StatusMessage: "This is a test message"}, nil
+}
+func (s *server) Login(ctx context.Context, request *pb.LoginRequest) (*pb.LoginResponse, error) {
+	return &pb.LoginResponse{StatusType: 1, StatusMessage: "This is a test message", OriginToken: "test", DerivativeToken: "test"}, nil
+}
+func (s *server) RefreshSession(ctx context.Context, request *pb.RefreshSessionRequest) (*pb.RefreshSessionResponse, error) {
+	return &pb.RefreshSessionResponse{StatusType: 1, StatusMessage: "This is a test message", DerivativeToken: "test"}, nil
+}
+func (s *server) CreateAccount(ctx context.Context, request *pb.CreateAccountRequest) (*pb.CreateAccountResponse, error) {
+	return &pb.CreateAccountResponse{StatusType: 1, StatusMessage: "This is a test message"}, nil
+}
+func (s *server) CreateAccount(ctx context.Context, request *pb.CreateAccountRequest) (*pb.CreateAccountResponse, error) {
+	return &pb.CreateAccountResponse{StatusType: 1, StatusMessage: "This is a test message"}, nil
+}
+func (s *server) CreateAccount(ctx context.Context, request *pb.CreateAccountRequest) (*pb.CreateAccountResponse, error) {
+	return &pb.CreateAccountResponse{StatusType: 1, StatusMessage: "This is a test message"}, nil
+}
+func (s *server) CreateAccount(ctx context.Context, request *pb.CreateAccountRequest) (*pb.CreateAccountResponse, error) {
+	return &pb.CreateAccountResponse{StatusType: 1, StatusMessage: "This is a test message"}, nil
+}
+func (s *server) CreateAccount(ctx context.Context, request *pb.CreateAccountRequest) (*pb.CreateAccountResponse, error) {
+	return &pb.CreateAccountResponse{StatusType: 1, StatusMessage: "This is a test message"}, nil
 }
 
 func main() {
@@ -31,5 +59,5 @@ func main() {
 	if err := s.Server(lis); err != nil {
 		log.Fatal("Failed to start server: %v", err)
 	}
-	log.Printf("Started server successfully on port %s", port);
+	log.Printf("Started server successfully on port %s", port)
 }
